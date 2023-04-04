5256edb9d58512eb938282983d2a501b54454e0a
Gabi-Boldea <gabriel.boldea02@gmail.com>
2023-04-04 23:36:58 +0300

    first commit

---
+++ TTT/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace TTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.btn_Iesire = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.scor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(104, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 50);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 50);
+            this.label2.TabIndex = 1;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(216, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 50);
+            this.label3.TabIndex = 2;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 50);
+            this.label4.TabIndex = 3;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(160, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 50);
+            this.label5.TabIndex = 4;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(216, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 50);
+            this.label6.TabIndex = 5;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(104, 237);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 50);
+            this.label7.TabIndex = 6;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(160, 237);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 50);
+            this.label8.TabIndex = 7;
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(216, 237);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 50);
+            this.label9.TabIndex = 8;
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label10.Location = new System.Drawing.Point(47, 127);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(282, 149);
+            this.label10.TabIndex = 9;
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.BackColor = System.Drawing.Color.Chocolate;
+            this.btn_Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_Reset.Location = new System.Drawing.Point(53, 399);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(86, 33);
+            this.btn_Reset.TabIndex = 10;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = false;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // btn_Iesire
+            // 
+            this.btn_Iesire.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn_Iesire.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_Iesire.Location = new System.Drawing.Point(232, 399);
+            this.btn_Iesire.Name = "btn_Iesire";
+            this.btn_Iesire.Size = new System.Drawing.Size(86, 33);
+            this.btn_Iesire.TabIndex = 11;
+            this.btn_Iesire.Text = "Iesire";
+            this.btn_Iesire.UseVisualStyleBackColor = false;
+            this.btn_Iesire.Click += new System.EventHandler(this.btn_Iesire_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(125, 466);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(122, 13);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "(C)2019 Groza Anamaria";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label12.BackColor = System.Drawing.Color.Tomato;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label12.Location = new System.Drawing.Point(-1, -1);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(408, 63);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "TIC TAC TOE";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scor
+            // 
+            this.scor.BackColor = System.Drawing.Color.Tomato;
+            this.scor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.scor.Location = new System.Drawing.Point(104, 332);
+            this.scor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.scor.Name = "scor";
+            this.scor.Size = new System.Drawing.Size(162, 41);
+            this.scor.TabIndex = 14;
+            this.scor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(407, 522);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.scor);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btn_Iesire);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.Button btn_Iesire;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label scor;
+    }
+}
+
---
+++ TTT/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label10.Visible = false;
+        }
+        int j = 1;//daca e impar X, par O
+        int counterX = 0;
+        int counterO = 0;
+        void verif()
+        {
+            if (label1.Text == label2.Text && label2.Text == label3.Text && label3.Text == "X"
+                || label4.Text == label5.Text && label5.Text == label6.Text && label6.Text == "X"
+                || label7.Text == label8.Text && label8.Text == label9.Text && label9.Text == "X"
+                || label1.Text == label4.Text && label4.Text == label7.Text && label7.Text == "X"
+                || label2.Text == label5.Text && label5.Text == label8.Text && label8.Text == "X"
+                || label3.Text == label6.Text && label6.Text == label9.Text && label9.Text == "X"
+                || label1.Text == label5.Text && label5.Text == label9.Text && label9.Text == "X"
+                || label3.Text == label5.Text && label5.Text == label7.Text && label7.Text == "X")
+            {
+                label10.Visible = true;
+                label10.Text = "A castgat X!";
+                counterX++;
+                scor.Text = "Punctaj: X=" + counterX.ToString() + " O=" + counterO.ToString();
+            }
+            else if (label1.Text == label2.Text && label2.Text == label3.Text && label3.Text == "O"
+                || label4.Text == label5.Text && label5.Text == label6.Text && label6.Text == "O"
+                || label7.Text == label8.Text && label8.Text == label9.Text && label9.Text == "O"
+                || label1.Text == label4.Text && label4.Text == label7.Text && label7.Text == "O"
+                || label2.Text == label5.Text && label5.Text == label8.Text && label8.Text == "O"
+                || label3.Text == label6.Text && label6.Text == label9.Text && label9.Text == "O"
+                || label1.Text == label5.Text && label5.Text == label9.Text && label9.Text == "O"
+                || label3.Text == label5.Text && label5.Text == label7.Text && label7.Text == "O")
+            {
+                label10.Visible = true;
+                label10.Text = "A castigat O!";
+                counterO++;
+                scor.Text = "Punctaj: X=" + counterX.ToString() + " O=" + counterO.ToString();
+            }
+
+            else if (label1.Text != "" && label2.Text != ""
+                && label3.Text != "" && label4.Text != ""
+                && label5.Text != "" && label6.Text != ""
+                && label7.Text != "" && label8.Text != "" && label9.Text != "")
+            {
+                label10.Visible = true;
+                label10.Text = "Remiza!";
+            }
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+            if (label1.Text == "")
+            {
+                if (j % 2 != 0)
+                    label1.Text = "X";
+                else
+                    label1.Text = "O";
+                j++;
+                verif();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (label2.Text == "")
+            {
+                if (j % 2 != 0)
+                    label2.Text = "X";
+                else
+                    label2.Text = "O";
+                j++;
+                verif();
+
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "")
+            {
+                if (j % 2 != 0)
+                    label3.Text = "X";
+                else
+                    label3.Text = "O";
+                j++;
+                verif();
+
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            if (label4.Text == "")
+            {
+                if (j % 2 != 0)
+                    label4.Text = "X";
+                else
+                    label4.Text = "O";
+                j++;
+                verif();
+
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            if (label5.Text == "")
+            {
+                if (j % 2 != 0)
+                    label5.Text = "X";
+                else
+                    label5.Text = "O";
+                j++;
+                verif();
+
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (label6.Text == "")
+            {
+                if (j % 2 != 0)
+                    label6.Text = "X";
+                else
+                    label6.Text = "O";
+                j++;
+                verif();
+
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (label7.Text == "")
+            {
+                if (j % 2 != 0)
+                    label7.Text = "X";
+                else
+                    label7.Text = "O";
+                j++;
+                verif();
+
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (label8.Text == "")
+            {
+                if (j % 2 != 0)
+                    label8.Text = "X";
+                else
+                    label8.Text = "O";
+                j++;
+                verif();
+
+            }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            if (label9.Text == "")
+            {
+                if (j % 2 != 0)
+                    label9.Text = "X";
+                else
+                    label9.Text = "O";
+                j++;
+                verif();
+
+            }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            label10.Visible = true;
+        }
+
+        private void btn_Iesire_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            j = 1;
+            label10.Visible = false;
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
